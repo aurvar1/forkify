c1d8f5850e4e1004f0ef0ceea98efb7fbacf274d
aurvar1 <aurelijus.var@gmail.com>
2025-02-12 09:30:20 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bb209531-a418-4b04-a0a9-674d00956205';
+export const MODAL_CLOSE_SEC = 2.5;
